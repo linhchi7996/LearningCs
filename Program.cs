c618e0045a496501b144bd4ba87f3c6928e0af81
TonyLe <tony.leviethung@gmail.com>
2023-06-05 14:47:50 +0700

    my first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace RainMessage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world");
+            Console.WriteLine("It's November!");
+            Console.WriteLine("Is it raining?");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# This is the program to print rain message.
